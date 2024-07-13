88a5bbe7b155c579c4647fcad5f87c78d19d9ef9
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2024-07-13 12:59:08 +0530

    add the feature of showing page transtions effects of all section

--- script.js
+++ script.js
@@ -7,11 +7,31 @@ function pageTransitions() {
     // Button click active class
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', function () {
-            let currentBtn = document.querySelector('.active-btn');
+            const currentBtn = document.querySelector('.active-btn');
             currentBtn.className = currentBtn.className.replace('active-btn', '');
             this.className += ' active-btn';
         });
     }
+
+    // sections active class 
+    allSections.addEventListener('click', function(e) {
+        const id = e.target.dataset.id
+        if(id) {
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            // hide other sections 
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id)
+            element.classList.add('active')
+            
+        }
+    })
 }
 
 pageTransitions();
